5aaa554cc9c1dd4f6fe2b9fae92bed6ed5dc5f5a
Joshua Sherman <joshua.v.sherman@gmail.com>
2019-04-06 19:19:30 -0400

    finishes nav on mountains page

--- src/mountains.js
+++ src/mountains.js
@@ -22,21 +22,61 @@ export class Mountains {
     const oLink = document.getElementsByClassName('oLink');
     this[name] = true;
     if (name === 'oldSection') {
-    // background-color:#760908;color:white;text-decoration:none
       oLink[0].style.backgroundColor = '#760908';
       oLink[0].style.color = 'white';
       oLink[0].style.textDecoration = 'none';
+      if (!this.widescreenPage) {
+        oLink[1].style.backgroundColor = '#760908';
+        oLink[1].style.color = 'white';
+        oLink[1].style.textDecoration = 'none';
+      }
+      // oLink[2].style.backgroundColor = '#760908';
+      // oLink[2].style.color = 'white';
+      // oLink[2].style.textDecoration = 'none';
       this.meruSection = false;
       this.killaSection = false;
       this.equipSection = false;
+      this.resetLinks(['kLink', 'mLink']);
     }
     if (name === 'killaSection') {
+      kLink[0].style.backgroundColor = '#760908';
+      kLink[0].style.color = 'white';
+      kLink[0].style.textDecoration = 'none';
+      if (!this.widescreenPage) {
+        kLink[1].style.backgroundColor = '#760908';
+        kLink[1].style.color = 'white';
+        kLink[1].style.textDecoration = 'none';
+      }
       this.meruSection = false;
       this.equipSection = true;
+      this.resetLinks(['oLink', 'mLink']);
     }
     if (name === 'meruSection') {
+      mLink[0].style.backgroundColor = '#760908';
+      mLink[0].style.color = 'white';
+      mLink[0].style.textDecoration = 'none';
+      if (!this.widescreenPage) {
+        mLink[1].style.backgroundColor = '#760908';
+        mLink[1].style.color = 'white';
+        mLink[1].style.textDecoration = 'none';
+      }
       this.killaSection = false;
       this.equipSection = true;
+      this.resetLinks(['kLink', 'oLink']);
+    }
+  }
+  resetLinks(nameArr) {
+    let elClass;
+    for (let i = 0; i < nameArr.length; i += 1) {
+      elClass = document.getElementsByClassName(nameArr[i]);
+      elClass[0].style.backgroundColor = 'white';
+      elClass[0].style.color = '#007bff';
+      elClass[0].style.textDecoration = 'underline';
+      if (!this.widescreenPage) {
+        elClass[1].style.backgroundColor = 'white';
+        elClass[1].style.color = '#007bff';
+        elClass[1].style.textDecoration = 'underline';
+      }
     }
   }
 }
