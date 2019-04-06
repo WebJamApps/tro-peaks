62e0412d0046bd6ad61185ab8559702cbe4557bc
Joshua Sherman <joshua.v.sherman@gmail.com>
2019-04-06 18:06:14 -0400

    wip switching mountains

--- src/mountains.js
+++ src/mountains.js
@@ -6,8 +6,37 @@ import {
 } from './app';
 
 @inject(App)
-export class Booking {
-  constructor(app) { this.app = app; }
+export class Mountains {
+  constructor(app) {
+    this.app = app;
+    this.killaSection = true;
+    this.meruSection = false;
+    this.oldSection = false;
+    this.equipSection = true;
+  }
   get widescreenPage() { return document.documentElement.clientWidth > 900; }
   attached() { document.getElementById('top').scrollIntoView(); }
+  showSection(name) {
+    const mLink = document.getElementsByClassName('mLink');
+    const kLink = document.getElementsByClassName('kLink');
+    const oLink = document.getElementsByClassName('oLink');
+    this[name] = true;
+    if (name === 'oldSection') {
+    // background-color:#760908;color:white;text-decoration:none
+      oLink[0].style.backgroundColor = '#760908';
+      oLink[0].style.color = 'white';
+      oLink[0].style.textDecoration = 'none';
+      this.meruSection = false;
+      this.killaSection = false;
+      this.equipSection = false;
+    }
+    if (name === 'killaSection') {
+      this.meruSection = false;
+      this.equipSection = true;
+    }
+    if (name === 'meruSection') {
+      this.killaSection = false;
+      this.equipSection = true;
+    }
+  }
 }
