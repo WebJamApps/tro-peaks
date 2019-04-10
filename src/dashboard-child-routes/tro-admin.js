f18196ab3374ec3c032bcb8cb35d0baca9db52c3
Joshua Sherman <jsherman@valcom.com>
2019-04-10 13:37:09 -0400

    deletes testimonials

--- src/dashboard-child-routes/tro-admin.js
+++ src/dashboard-child-routes/tro-admin.js
@@ -28,111 +28,21 @@ export class ClcAdmin {
     this.showDeleteButton = false;
     this.troHomePageContent = { title: '', comments: '', type: 'troHomePageContent' };
     this.newTestimonial = { title: '', comments: '', type: 'troTestimonials' };
-    // this.youthPageContent = { title: '', comments: '', type: 'youthPageContent' };
-    // this.familyPageContent = { title: '', comments: '', type: 'familyPageContent' };
-    // this.youthPicsArr = [];
-    // this.familyPicsArr = [];
   }
 
-  // types = ['Monthly'];
   get widescreenPage() { return document.documentElement.clientWidth > 900; }
   async activate() {
     this.app.dashboardTitle = 'Admin';
     const uid = this.app.auth.getTokenPayload().sub;
     this.user = await this.app.appState.getUser(uid);
     this.app.role = this.user.userType;
-    // this.types.sort();
-    // await this.setupValidation();
     let res;
     try {
-      res = await this.app.httpClient.fetch('/book');
+      res = await this.app.httpClient.fetch('/book?type=troTestimonials');
       if (res !== null && res !== undefined) this.existingBooks = await res.json();
-    } catch (e) { return sessionStorage.setItem('adminError', `${e.message}`); }// eslint-disable-line no-console
-    return this.fixBooks(this.existingBooks);
+    } catch (e) { return sessionStorage.setItem('testimonialsError', `${e.message}`); }
+    return Promise.resolve(true);
   }
-  fixBooks(books) {
-    this.existingBooks = this.utils.filterNews(books);
-    // this.youthPicsArr = [];
-    // this.familyPicsArr = [];
-    // for (let i = 0; i < books.length; i += 1) {
-    //   if (books[i].type === 'youthPics') this.youthPicsArr.push(books[i]);
-    //   if (books[i].type === 'familyPics') this.familyPicsArr.push(books[i]);
-    // }
-  }
-  // showDelete() {
-  //   this.showDeleteButton = true;
-  //   if (this.titleSelected === '') this.showDeleteButton = false;
-  // }
-  // deleteBook() {
-  //   const selectBookTitle = document.getElementById('selectBookTitle');
-  //   const id = selectBookTitle.options[selectBookTitle.selectedIndex].value;
-  //   return this.utils.deleteBookById(id, this, 'news');
-  // }
-  // async deleteYouthPic() {
-  //   const selectYouthPic = document.getElementById('selectYouthPic');
-  //   const id = selectYouthPic.options[selectYouthPic.selectedIndex].value;
-  //   return this.utils.deleteBookById(id, this, 'youth');
-  // }
-  // async deleteFamilyPic() {
-  //   const selectFamilyPic = document.getElementById('selectFamilyPic');
-  //   const id = selectFamilyPic.options[selectFamilyPic.selectedIndex].value;
-  //   return this.utils.deleteBookById(id, this, 'family');
-  // }
-  // setupValidation() {
-  //   ValidationRules
-  //     .ensure('url').required().withMessage('URL to PDF is required')
-  //     .ensure('type').required().withMessage('Select Weekly or Monthly')
-  //     .on(this.newBook);
-  //   return Promise.resolve(true);
-  // }
-  // updateCanSubmit(validationResults) {
-  //   let valid = true;
-  //   const nub = document.getElementById('createMediaButton');
-  //   nub.style.display = 'none';
-  //   for (const result of validationResults) {
-  //     if (result.valid === false) {
-  //       valid = false;
-  //       nub.style.display = 'none';
-  //       break;
-  //     }
-  //   }
-  //   this.canSubmit = valid;
-  //   if (this.canSubmit && this.newBook.type !== 0) {
-  //     nub.style.display = 'block';
-  //   }
-  //   return this.canSubmit;
-  // }
-  // setTitle() {
-  //   this.newBook.title = this.newBook.url;
-  //   let urlArr = this.newBook.title.split('/');
-  //   urlArr = urlArr[5].split('.pdf');
-  //   this.newBook.title = urlArr[0].replace('%20', '_');
-  //   const spacesArr = this.newBook.title.split('%20');
-  //   for (let i = 0; i < spacesArr.length; i += 1) this.newBook.title = this.newBook.title.replace('%20', '_');
-  //   this.newBook.title = `${this.newBook.title}.pdf`;
-  //   return Promise.resolve(true);
-  // }
-  // fixUrl() {
-  //   this.newBook.url = this.newBook.url.replace('www.dropbox', 'dl.dropboxusercontent');
-  //   return Promise.resolve(true);
-  // }
-  // async createBook() {
-  //   await this.setTitle();
-  //   await this.fixUrl();
-  //   return this.utils.createBook(this, 'news', json);
-  // }
-  // async createYouthPic() {
-  //   await this.fixUrl();
-  //   this.newBook.type = 'youthPics';
-  //   this.newBook.title = 'youthPics';
-  //   return this.utils.createBook(this, 'youth', json);
-  // }
-  // async createFamilyPic() {
-  //   await this.fixUrl();
-  //   this.newBook.type = 'familyPics';
-  //   this.newBook.title = 'familyPics';
-  //   return this.utils.createBook(this, 'family', json);
-  // }
   async changeHomePage() {
     this.app.httpClient.fetch('/book/one?type=troHomePageContent', {
       method: 'put',
@@ -143,6 +53,7 @@ export class ClcAdmin {
       });
   }
   async addTestimonial() {
+    if (this.newTestimonial.title === '') this.newTestimonial.title = 'Anonymous';
     this.app.httpClient.fetch('/book', {
       method: 'post',
       body: json(this.newTestimonial)
@@ -151,22 +62,13 @@ export class ClcAdmin {
         this.app.router.navigate('/testimonials?reload=true');
       });
   }
-  // async changeYouthPage() {
-  //   this.app.httpClient.fetch('/book/one?type=youthPageContent', {
-  //     method: 'put',
-  //     body: json(this.youthPageContent)
-  //   })
-  //     .then(() => {
-  //       this.app.router.navigate('/youth?reload=true');
-  //     });
-  // }
-  // async changeFamilyPage() {
-  //   this.app.httpClient.fetch('/book/one?type=familyPageContent', {
-  //     method: 'put',
-  //     body: json(this.familyPageContent)
-  //   })
-  //     .then(() => {
-  //       this.app.router.navigate('/family?reload=true');
-  //     });
-  // }
+  showDelete() {
+    this.showDeleteButton = true;
+    if (this.titleSelected === '') this.showDeleteButton = false;
+  }
+  deleteBook() {
+    const selectBookTitle = document.getElementById('selectBookTitle');
+    const id = selectBookTitle.options[selectBookTitle.selectedIndex].value;
+    return this.utils.deleteBookById(id, this, 'testimonials');
+  }
 }
